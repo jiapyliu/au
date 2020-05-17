054d00925b262e4116771e7db30a0c1bc0fb7437
o <1691315371@qq.com>
2020-05-17 14:51:35 +0800

    inti

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 import sys
+import os
 from setuptools import setup
+import pprint
 
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here,'au','__version__.py'),'r',encoding='utf-8') as f:
+    exec (f.read(),about)
 
-packages = ['requests']
-requires = [
+pprint.pprint(about)
 
-]
-
+packages = ['au']
+# requires = [
+#
+# ]
+#
 setup(
     name = 'au',
     packages = packages,
-    author=about['__author__']
+    version=about['__version__'],
+    description=about['__description__'],
+
+    project_urls={
+        'Documentation': 'https://github.com/jiapyliu/au',
+        'Source': 'https://github.com/jiapyliu/au',
+    },
 )
