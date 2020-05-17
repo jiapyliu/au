4493ee26c26050b5cf28be59ef27bc878ed5d841
o <1691315371@qq.com>
2020-05-17 11:49:04 +0800

    inti

--- eda/matplotlib_util.py
+++ eda/matplotlib_util.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def show_image(image:np.ndarray):
+
+def show_image(image: np.ndarray):
     plt.imshow(image)
     plt.show()
 
-#https://www.kaggle.com/retfing/alaska2-eda-and-efficientnet/edit
+
+# https://www.kaggle.com/retfing/alaska2-eda-and-efficientnet/edit
 def plot_like():
-
     fig, axes = plt.subplot(nrow=4, ncols=4, figsize=(11, 11))
-
 
     pass
