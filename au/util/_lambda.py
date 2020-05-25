fa5250da94794e9e875d4d1bc6b5fc78b618706e
o <1691315371@qq.com>
2020-05-25 15:17:13 +0800

    inti

---
+++ au/util/_lambda.py
@@ -0,0 +1,8 @@
+
+
+_lambda_dict_nlp = {
+    # max_seq_length : https://blog.csdn.net/cyong888/article/details/94397458
+    'max_seq_length' : lambda x:x
+}
+
+print(_lambda_dict_nlp['max_seq_length'](1))
